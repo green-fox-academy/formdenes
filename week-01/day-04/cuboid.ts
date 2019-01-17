7f98070cdaa18e744c8b0371663267988fed8cc5
Formanek Denes <formdenes@gmail.com>
2019-01-17 16:12:50 +0100

    Added cuboid.ts

---
+++ week-01/day-04/cuboid.ts
@@ -0,0 +1,9 @@
+'use strict';
+export{};
+
+var a: number = 10;
+var b: number = 20;
+var c: number = 30;
+
+console.log("Surface Area: " + 2 * ((a * b) + (a * c) + (b*c)));
+console.log("Volume: " + a*b*c);
