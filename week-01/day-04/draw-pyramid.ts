ba783471ae7462c78f2ff71e30576bae4984e3ed
Formanek Denes <formdenes@gmail.com>
2019-01-17 17:29:47 +0100

    Added draw-pyramid.ts

---
+++ week-01/day-04/draw-pyramid.ts
@@ -0,0 +1,15 @@
+'use strict';
+export{};
+
+let lineCount: number = 4;
+let print: string = "";
+for (let i: number = 1; i<= lineCount; i++){
+    for(let j: number = 0; j<lineCount-i; j++){
+        print = print + ' ';
+    }
+    for (let j: number = 0; j<2*(i-1)+1; j++){
+        print = print + '*';
+    }
+    console.log(print);
+    print = "";
+}
