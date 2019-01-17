d00fda90cc107155f1ebcf1f637b012b7d2b5b55
Formanek Denes <formdenes@gmail.com>
2019-01-17 15:29:35 +0100

    Added define-basicc-info.ts

---
+++ week-01/day-04/define-basic-info.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+let myName: string = "Denes";
+let myAge: number = 23;
+let myHeight: number = 1.925;
+let amIMarryed: boolean = false;
+console.log(myName);
+console.log(myAge);
+console.log(myHeight);
+console.log(amIMarryed);
