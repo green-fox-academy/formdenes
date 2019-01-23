6cd50758a87e58c23070f59ef763c1d167930827
formdenes <formdenes@gmial.com>
2019-01-23 14:00:47 +0100

    Added center-box-function files

---
+++ week-02/day-03/center-box-function-ts.ts
@@ -0,0 +1,29 @@
+'use strict';
+export{};
+
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Create a square drawing function that takes 1 parameter:
+// The square size
+// and draws a square of that size to the center of the canvas.
+// Draw 3 squares with that function.
+// Avoid code duplication.
+
+function randomColor(){
+    let r = function () { return Math.floor(Math.random()*256) };
+    return "rgb(" + r() + "," + r() + "," + r() + ")";
+}
+
+
+function drawSquare(a: number){
+    ctx.fillStyle = randomColor();
+    ctx.fillRect(300-(a/2),200-(a/2),a,a);
+}
+
+for (let i:number = 0; i<3; i++){
+    drawSquare(Math.random()*200);
+}
