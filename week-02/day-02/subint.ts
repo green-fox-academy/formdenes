b91cd41b34ab4525cf36400a59592db1147fb388
formdenes <formdenes@gmial.com>
2019-01-23 11:10:08 +0100

    Added the optional exercises from week-02/day-02

---
+++ week-02/day-02/subint.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+
+function subint (value:number, list: number[]){
+    let foundNumbers:number[] = list.filter((numb, index) => {
+        let str:string = numb.toString()
+        if (numb.toString().indexOf(value.toString())!==-1){
+            return value;
+        }
+    });
+    return foundNumbers.map(num => list.indexOf(num));
+    
+}
+
+console.log(subint(1, [1, 11, 34, 52, 61]));
+console.log(subint(9, [1, 11, 34, 52, 61]));
+
