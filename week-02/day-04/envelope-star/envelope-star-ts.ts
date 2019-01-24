a167afc62df0cbf800fce2214c13cc6185d3763d
formdenes <formdenes@gmial.com>
2019-01-24 13:37:31 +0100

    Prepare for the envelope star

--- week-02/day-04/envelope-star/envelope-star-ts.ts
+++ week-02/day-04/envelope-star/envelope-star-ts.ts
@@ -8,7 +8,6 @@ const ctx = canvas.getContext('2d');
 function randomColor(){
     let r = function () { return Math.floor(Math.random()*256) };
     return "rgb(" + r() + "," + r() + "," + r() + ")";
-
 }
 
 function drawLine(x1:number, y1:number, x2:number, y2:number, color: string){
@@ -19,29 +18,23 @@ function drawLine(x1:number, y1:number, x2:number, y2:number, color: string){
     ctx.stroke();
 }
 
-function drawP(startx:number, starty:number, size: number, density:number, color:string[]){
-    for (let i: number = 0; i<2; i++){
-        for (let j: number = 0;j<density; j++){
-            drawLine(i*j*(size/density)+startx,(1-i)*j*(size/density)+starty,(1-i)*j*(size/density)+(i*size)+startx,(i)*j*(size/density)+((1-i)*size)+starty,color[i]);
+function envStar(size: number, density:number, color:string){
+    for (let i: number = 0; i<4; i++){
+        let evens: number = i%2;
+        let sign:number = Math.sign(1.5-i);
+        for (let j: number = 0;j<=density; j++){
+            drawLine(evens*size+(1-evens)*j*size/density, (1-sign*evens)*size+sign*evens*j*size/density, size+evens*j*size/density, size-sign*(1-evens)*j*size/density,color);
         }
     }
 }
 
 //const values
 const canvasSize:number = 900;
-const div: number = 30;
+const div: number = 2;
 const width:number = canvasSize/div;
 
 //dynamic density
 let lineCount:number =Math.max( 600/(div*10),10);
 
-//Setting up the two colors
-let colors:string[] = ['',''];
-colors = colors.map(str => randomColor());
-
-//Drawings
-for(let rows:number = 0;rows<div;rows++){
-    for (let columns:number = 0; columns<div; columns++){
-        drawP(columns*width, rows*width, width, lineCount, colors);
-    }
-}
+//Do the things
+envStar(width,lineCount,randomColor());
