2780fb1f2c43437ac1dba0fe24ee56ae140b0d00
formdenes <formdenes@gmial.com>
2019-02-06 11:44:24 +0100

    Finished domino comparator task

---
+++ week-04/day-03/domino.ts
@@ -0,0 +1,26 @@
+interface Comparable {
+    compareTo(other: Comparable): number;
+    /*
+    * returns negative number if this is smaller than other
+    * returns 0 if they are the same
+    * returns positive number if this is greater than other
+    */
+  }
+
+class Domino implements Comparable {
+    values: number[];
+    constructor(valueA: number, valueB: number) {
+        this.values = [valueA, valueB];
+    }
+    compareTo(other:Domino):number{
+        if (this.values[0] < other.values[0]) {
+            return -1;
+          }
+        else if (this.values[0] > other.values[0]) {
+            return 1;
+        }
+        else return 0;
+    }
+}
+
+export {Domino};
