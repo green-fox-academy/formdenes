e765dceea70c6ca61940085517c899647651e4f1
formdenes <formdenes@gmial.com>
2019-02-07 15:25:23 +0100

    Created fuck up test

--- week-04/day-04/extension.test.ts
+++ week-04/day-04/extension.test.ts
@@ -3,13 +3,13 @@
 import { test } from 'tape';
 import { add, maxOfThree, median, isVowel, translate } from './extension';
 
-test('add: 2 and 3 is 5', function (t: any): any {
-  t.equal(add(2, 3), 5);
+test('add: 2 and 2 is 4', function (t: any): any {
+  t.equal(add(2, 2), 4);
   t.end();
 });
 
-test('add: 1 and 4 is 5', function (t: any): any {
-  t.equal(add(1, 4), 5);
+test('add: 1 and 3 is 4', function (t: any): any {
+  t.equal(add(1, 3), 4);
   t.end();
 });
 
@@ -19,12 +19,12 @@ test('max of three: first', function (t: any): any {
 });
 
 test('max of three: third', function (t: any): any {
-  t.equal(maxOfThree(3, 4, 5), 5);
+  t.equal(maxOfThree(4, 3, 5), 5);
   t.end();
 });
 
 test('median: four', function (t: any): any {
-  t.equal(median([7, 5, 3, 5]), 5);
+  t.equal(median([7, 5, 3, 4]), 4.5);
   t.end();
 });
 
@@ -33,27 +33,27 @@ test('median: five', function (t: any): any {
   t.end();
 });
 
-test('is vowel: a', function (t: any): any {
-  t.ok(isVowel('a'));
+test('is vowel:  é', function (t: any): any {
+  t.ok(isVowel('é'));
   t.end();
 });
 
-test('is vowel: u', function (t: any): any {
-  t.ok(isVowel('u'));
+test('is vowel: í', function (t: any): any {
+  t.ok(isVowel('í'));
   t.end();
 });
 
-test("is vowel: k", function(t: any): any {
-  t.notOk(isVowel("k"));
+test("is vowel: u", function(t: any): any {
+  t.notOk(isVowel("u"));
   t.end();
 });
 
-test('translate: bemutatkozik', function (t: any): any {
-  t.equal(translate('bemutatkozik'), 'bevemuvutavatkovozivik');
+test('translate: anyád', function (t: any): any {
+  t.equal(translate('anyád'), 'avanyávád');
   t.end();
 });
 
-test('translate: lagopus', function (t: any): any {
-  t.equal(translate('lagopus'), 'lavagovopuvus');
+test('translate: fákmí', function (t: any): any {
+  t.equal(translate('fákmí'), 'fávákmíví');
   t.end();
 });
