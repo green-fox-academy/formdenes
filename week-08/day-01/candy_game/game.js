4ba3c5bb5756aa9dee3b332f29b9436477a24fc6
formdenes <formdenes@gmial.com>
2019-03-11 16:49:51 +0100

    candy game

---
+++ week-08/day-01/candy_game/game.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const lollypopPrice = 100;
+
+const createCandies = document.querySelector('.create-candies');
+const buyLollypops = document.querySelector('.buy-lollypops');
+const candyMachine = document.querySelector('.candy-machine');
+
+const candies = document.querySelector('.candies');
+const lollipops = document.querySelector('.lollypops');
+const speed = document.querySelector('.speed');
+
+let multiplyer = 1;
+
+let numberOfCandies = 0;
+let numberOfLollipops = 0;
+let productionRate = Math.floor(numberOfLollipops/10);
+
+const getLollypopText = () => {
+  let text = '';
+  for(let i = 0; i<numberOfLollipops; i++){
+    text += '🍭';
+  }
+  return text;
+}
+
+const giveCandy = () => {
+  numberOfCandies+= 1*multiplyer;
+  candies.textContent = numberOfCandies;
+}
+
+const buyLollypop = () => {
+  numberOfCandies>=lollypopPrice?
+  (numberOfCandies-=lollypopPrice,
+  numberOfLollipops++,
+  candies.textContent = numberOfCandies)
+  :
+  null;
+  lollipops.textContent = getLollypopText();
+  productionRate = Math.floor(numberOfLollipops/10);
+  speed.textContent = productionRate;
+}
+
+setInterval(() => {
+  
+  productionRate = Math.floor(numberOfLollipops/10);
+  for (let i = 0; i<productionRate; i++){
+    giveCandy();
+  }
+},1000);
+
+candies.textContent = numberOfCandies;
+lollipops.textContent = numberOfLollipops;
+speed.textContent = productionRate;
+
+createCandies.onclick = giveCandy;
+buyLollypops.onclick = buyLollypop;
+
+candyMachine.onclick = () => {
+  multiplyer = multiplyer*10;
+  candyMachine.textContent = `Make candy rain x${multiplyer}`
+}
+
