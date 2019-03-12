1d73376df54d2c212335376e2480f3f98eeac428
formdenes <formdenes@gmial.com>
2019-03-12 16:43:21 +0100

    finished for today

--- week-08/day-02/labelled-buttons/app.js
+++ week-08/day-02/labelled-buttons/app.js
@@ -2,8 +2,13 @@
 
 const buttons = document.querySelector('.buttons');
 
+const deleteButton = buttons.querySelector('.delete-action');
+const deleteButtonLabel = buttons.querySelector('.delete-action .label');
+const checkButton = buttons.querySelector('.delete-check');
+const checkButtonLabel = buttons.querySelector('.delete-check .label');
 buttons.querySelector('.delete-option').addEventListener('mouseenter', (e) => {
-  //console.log(e.target);
+  deleteButton.classList.remove('hide');
+  deleteButtonLabel.classList.remove('hide');
   if (buttons.querySelector('.delete-action').classList.contains('hide')){
     buttons.querySelector('.delete-action').classList.remove('hide');
   }
@@ -11,17 +16,16 @@ buttons.querySelector('.delete-option').addEventListener('mouseenter', (e) => {
 
 buttons.querySelector('.delete-action').addEventListener('click', (e) => {
   //console.log(e.target);
-  if (buttons.querySelector('.delete-check').classList.contains('hide')){
-    buttons.querySelector('.delete-check').classList.remove('hide');
-  }
+  checkButton.classList.remove('hide');
+  checkButtonLabel.classList.remove('hide')
 });
 
 buttons.onclick = (e) => {
   console.log ();
   if (e.target.parentElement.classList.contains('delete-option') || e.target.parentElement.parentElement.classList.contains('delete-option')){
-    const deleteB = buttons.querySelector('.delete-action');
-    const checkB = buttons.querySelector('.delete-check');
-    checkB.classList.add('hide');
-    deleteB.classList.toggle('hide');
+    checkButton.classList.add('hide');
+    checkButtonLabel.classList.add('hide');
+    deleteButton.classList.toggle('hide');
+    deleteButtonLabel.classList.toggle('hide');
   }
 }
